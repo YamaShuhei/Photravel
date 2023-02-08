251d2250b1a2851baefe4910d4d489e2457a3cb7
YamaShuhei <plg.function06@gmail.com>
2023-02-08 19:59:49 +0000

    Install devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  ##デバイス機能実行前にconfigure_permitted_parametersを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+    
+    
+    protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,27 @@
 Rails.application.routes.draw do
+  #Devise関連のルーティング
+  ##管理者用
+  devise_for :admin,skip:[:registrations, :passwords], controllers: {
+  sessions: "admin/sessions"
+  }
+  ##ユーザー用
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-get '/' => "homes#top"
+  
+# ユーザー側のルーティング設定
+scope module: :public do
+  root to: 'homes#top'
+  resources :posts
+end
+
+#管理者側のルーティング設定
+namespace :admin do
+  root to: "homes#top"
+  resources :posts, only:[:show, :destroy]
+  resources :users, only:[:show]
+end
 
 end
