bfc07172f2156d140705d4df32ffb2e96a9ad7a4
YamaShuhei <plg.function06@gmail.com>
2023-02-25 20:50:56 +0000

    fix homepage

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,7 +2,7 @@
 
 class Public::HomesController < ApplicationController
   def top
-    @posts = Post.all.order(created_at: "DESC").limit(10)
+    @posts = Post.all.order(created_at: "DESC").limit(20)
     @month_favorite_ranks = Post.find(Favorite.group(:post_id).where(created_at: Time.current.all_month).order("count(post_id) desc").limit(5).pluck(:post_id))
   end
 end
